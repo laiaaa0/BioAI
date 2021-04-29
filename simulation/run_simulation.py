e0b6ada23f0d3c45ea6d1f530615b7605f855b61
Freddie Turner <jx20025@bristol.ac.uk>
2021-04-29 11:35:33 +0100

    Added default values for num_iterations (=100) and num_agents (=50). Added a testing flag (hardcoded True/False) to switch on/off trench and random extinguishing behaviour seen in simulation.

--- simulation/run_simulation.py
+++ simulation/run_simulation.py
@@ -3,24 +3,29 @@ from simulation.arena import Arena
 import itertools
 import random
 
-def run(network=None, show_plot=True):
+testing = True
+
+def run(network=None, num_iterations=100, num_agents=50, show_plot=True):
     # Specifying fire starting locations
     init_fire = [(0,0), (20,20), (50,50), (50,51)]
 
-    a = Arena(init_fire, num_agents=100)
+    a = Arena(init_fire, num_agents, network)
+
     # Set up trench
-    a.add_trench(itertools.product([70], range(30, 70)))
-    num_iterations = 50
+    if testing:
+        a.add_trench(itertools.product([70], range(30, 70)))
+
     for i in range(num_iterations):
         if show_plot:
             a.plot()
         a.update()
 
         # Randomly extinguish at a few (100) places
-        extinguish_coords = []
-        for i in range(100):
-            extinguish_coords.append((random.randint(0,99), random.randint(0,99)))
-        a.extinguish(extinguish_coords)
+        if testing:
+            extinguish_coords = []
+            for i in range(100):
+                extinguish_coords.append((random.randint(0,99), random.randint(0,99)))
+            a.extinguish(extinguish_coords)
 
     if show_plot:
         print("Simulation finished")
@@ -28,4 +33,6 @@ def run(network=None, show_plot=True):
     return a.get_fitness_function()
 
 if __name__=="__main__":
-    print(run(None,True))
+    #print(run(None, 100, 50, True))
+    # Testing default parameters
+    print(run())
